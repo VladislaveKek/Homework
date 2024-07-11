96b9cb431f8e0c3bf90701f0cb47b320e87e9d87
{VladislaveKek} <{vgrolyg@yandex.ru}>
2024-07-11 22:44:36 +0300

    Это база, основа, фундамент

---
+++ src/main/java/otus/free/FreeClass.java
@@ -0,0 +1,7 @@
+package otus.free;
+
+public class FreeClass {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/otus/homework/HomeworkClass.java
@@ -0,0 +1,4 @@
+package otus.homework;
+
+public class HomeworkClass {
+}
