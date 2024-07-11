dcd544c3125c18f593ab43d338ac7c448790a92a
{VladislaveKek} <{vgrolyg@yandex.ru}>
2024-07-12 01:19:19 +0300

    43 строка, заменил else на if, тестим

--- src/main/java/otus/homework/HomeworkClass.java
+++ src/main/java/otus/homework/HomeworkClass.java
@@ -3,20 +3,28 @@ package otus.homework;
 import java.util.Scanner;
 
 public class HomeworkClass {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("С чего начнем?");
         System.out.println("1. Выбор персонажа!");
         System.out.println("2. Выбор характеристик!");
         System.out.println("3. В бой!");
-
+        System.out.println("4. ");
+        System.out.println("5. ");
 
         Scanner scanner = new Scanner(System.in);
         int menu = scanner.nextInt();
-        if (menu == 1) { greetings();}
-        if (menu == 2) { checkSign();}
-        if (menu == 3) { selectorColor();}
+        if (menu == 1) {
+            greetings(); }
+        if (menu == 2) {
+            checkSign(); }
+        if (menu == 3) {
+            selectorColor(); }
+        if (menu == 4) {
+            compareNumbers(); }
+        if (menu == 5) {
+            addOrSubtractAndPrint(); }
     }
-    public static void greetings (){
+    public static void greetings() {
         System.out.println("Кто ты?");
         System.out.println("1. ГОРК!");
         System.out.println("2. МОРК!");
@@ -26,17 +34,18 @@ public class HomeworkClass {
         int character = scanner.nextInt();
         if (character == 1) {
             System.out.println("ЗА ГОРКА!");
-        } else if (character == 2) {
+        } if (character == 2) {
             System.out.println("ЗА МОРКА!");
-        } else if (character == 3) {
+        } if (character == 3) {
             System.out.println("WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGH");
-        } else if (character == 4) {
+        } if (character == 4) {
             System.out.println("Бей его ребята!");
-        } else {
+        } if {
             System.out.println("Попробуй еще раз");
         }
     }
-    public static void checkSign (){
+
+    public static void checkSign() {
         System.out.println("Сколько у тебя очков силы?");
         Scanner scanner = new Scanner(System.in);
         int power = scanner.nextInt();
@@ -44,26 +53,36 @@ public class HomeworkClass {
         int intelligence = scanner.nextInt();
         System.out.println("А сколько у тебя жубов?");
         int jubs = scanner.nextInt();
-        int result = power+intelligence+jubs;
-        System.out.print("Твой рейтинг в орде = " );
+        int result = power + intelligence + jubs;
+        System.out.print("Твой рейтинг в орде = ");
         System.out.println(result);
-        if (result >=0) {
+        if (result >= 0) {
             System.out.println("Ты настоящий орк!");
         }
         if (result < 0) {
             System.out.println("Человечешка! Бей его ребята!");
         }
     }
-    public static void selectorColor () {
-        System.out.println("В этой схватке ты победил = ");
+
+    public static void selectorColor() {
         int enemies = (int) (Math.random() * 100);
+        System.out.print("Ты победил ");
+        System.out.print(enemies);
+        System.out.println(" человешек!");
         if (enemies <= 30) {
-            System.out.println("Есть куда расти");
+            System.out.println("Это мало");
         }
         if (enemies > 30 && enemies <= 60) {
-            System.out.println("Неплохо!");
+            System.out.println("Это средне");
         }
         if (enemies > 60) {
-            System.out.println("WAAAAGH!");
+            System.out.println("Это много!");
         }
-    }}
+    }
+    public static void compareNumbers() {
+
+    }
+    public static void addOrSubtractAndPrint() {
+
+    }
+}
