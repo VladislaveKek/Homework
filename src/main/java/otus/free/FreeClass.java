a2996fe3c120134694f75a196f05281b63433d15
{VladislaveKek} <{vgrolyg@yandex.ru}>
2024-08-17 12:57:16 +0300

    Подготовка к рабочему процессу

--- src/main/java/otus/free/FreeClass.java
+++
@@ -1,7 +0,0 @@
-package otus.free;
-
-public class FreeClass {
-    public static void main (String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- src/main/java/otus/homework/HomeworkClass.java
+++ src/main/java/otus/homework/HomeworkClass.java
@@ -1,7 +1,9 @@
 package otus.homework;
 
+import java.util.Arrays;
+
 public class HomeworkClass {
-    public static void main (String[] args) {
-        System.out.println("Hello Merge");
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
